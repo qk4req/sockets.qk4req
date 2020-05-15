685bd93da114c0e66f512afe466a94f8f6a1af66
Qk4req <qk4req@gmail.com>
2020-05-15 20:07:38 +0300

    add jwt auth in sockets

--- listeners/broadcastListener.js
+++ listeners/broadcastListener.js
@@ -1,6 +1,7 @@
 module.exports = function (io) {
 	const broadcast = io.of('/broadcast');
 	broadcast.on('connection', (socket) => {
+		console.log('Authentication passed!');
 		//socket.emit('ready', );
 		//socket.on('', function() {
 			
