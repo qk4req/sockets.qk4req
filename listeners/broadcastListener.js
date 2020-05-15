22e16abea13e101ca22e7df117bbe27be46adab8
Qk4req <qk4req@gmail.com>
2020-05-15 15:28:35 +0300

    com conn broadcast

--- listeners/broadcastListener.js
+++ listeners/broadcastListener.js
@@ -1,9 +1,9 @@
 module.exports = function (io) {
 	const stream = io.of('/broadcast');
 	stream.on('connection', (socket) => {
-		socket.emit('ready', );
-		socket.on('', function() {
+		//socket.emit('ready', );
+		//socket.on('', function() {
 			
-		});
+		//});
 	});
 }
